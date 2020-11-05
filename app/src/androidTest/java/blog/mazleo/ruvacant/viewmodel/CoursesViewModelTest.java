fbdb4cabed30a9fc08138009332c5dede3904348
Jonnelin Marzielli Leonardo <jonnelin.leonardo@gmail.com>
2020-11-04 20:21:15 -0500

    Add additional data validations

--- app/src/androidTest/java/blog/mazleo/ruvacant/viewmodel/CoursesViewModelTest.java
+++ app/src/androidTest/java/blog/mazleo/ruvacant/viewmodel/CoursesViewModelTest.java
@@ -33,12 +33,16 @@ public class CoursesViewModelTest {
                 "U"
         ));
 
-        try {
-            Thread.sleep(60000);
-        }
-        catch (InterruptedException e) {
-            e.printStackTrace();
-            return;
+        final int NUM_SECONDS_WAIT = 60;
+        for (int s = 0; s < NUM_SECONDS_WAIT; s++) {
+            try {
+                Thread.sleep(1000);
+                Log.i("APPDEBUG", "SEC: " + (s + 1));
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+                return;
+            }
         }
 
         List<Subject> subjects = coursesViewModel.getSubjects();
@@ -80,5 +84,88 @@ public class CoursesViewModelTest {
         Assert.assertThat(instructors.size(), Matchers.greaterThan(0));
         Assert.assertThat(classesInstructors.size(), Matchers.greaterThan(0));
         Assert.assertThat(meetings.size(), Matchers.greaterThan(0));
+
+        assertAllSubjectDataValid(subjects);
+        assertAllCourseDataValid(courses);
+        assertAllClassDataValid(classes);
+        assertAllInstructorDataValid(instructors);
+        assertAllClassInstructorDataValid(classesInstructors);
+        assertAllMeetingDataValid(meetings);
+        assertAllMeetingTimesValid(meetings);
+    }
+
+    private void assertAllMeetingDataValid(List<Meeting> meetings) {
+        for (Meeting meeting : meetings) {
+            Assert.assertNotNull(meeting.getClassIndex());
+            Assert.assertNotNull(meeting.getBuildingCode());
+            Assert.assertNotNull(meeting.getRoomNumber());
+            Assert.assertNotNull(meeting.getMeetingDay());
+
+            Assert.assertThat(meeting.getClassIndex().length(), Matchers.greaterThan(0));
+            Assert.assertThat(meeting.getBuildingCode().length(), Matchers.greaterThan(0));
+            Assert.assertThat(meeting.getRoomNumber().length(), Matchers.greaterThan(0));
+            Assert.assertThat(meeting.getMeetingDay().length(), Matchers.greaterThan(0));
+        }
+    }
+
+    private void assertAllClassInstructorDataValid(List<ClassInstructor> classesInstructors) {
+        for (ClassInstructor classInstructor : classesInstructors) {
+            Assert.assertNotNull(classInstructor.getClassIndex());
+            Assert.assertNotNull(classInstructor.getInstructorLastName());
+
+            Assert.assertThat(classInstructor.getClassIndex().length(), Matchers.greaterThan(0));
+            Assert.assertThat(classInstructor.getInstructorLastName().length(), Matchers.greaterThan(0));
+        }
+    }
+
+    private void assertAllInstructorDataValid(List<Instructor> instructors) {
+        for (Instructor instructor : instructors) {
+            Assert.assertNotNull(instructor.getLastName());
+
+            Assert.assertThat(instructor.getLastName().length(), Matchers.greaterThan(0));
+        }
+    }
+
+    private void assertAllClassDataValid(List<Class> classes) {
+        for (Class clazz : classes) {
+            Assert.assertNotNull(clazz.getIndex());
+            Assert.assertNotNull(clazz.getCode());
+            Assert.assertNotNull(clazz.getSubjectCode());
+            Assert.assertNotNull(clazz.getCourseCode());
+
+            Assert.assertThat(clazz.getIndex().length(), Matchers.greaterThan(0));
+            Assert.assertThat(clazz.getCode().length(), Matchers.greaterThan(0));
+            Assert.assertThat(clazz.getSubjectCode().length(), Matchers.greaterThan(0));
+            Assert.assertThat(clazz.getCourseCode().length(), Matchers.greaterThan(0));
+        }
+    }
+
+    private void assertAllCourseDataValid(List<Course> courses) {
+        for (Course course : courses) {
+            Assert.assertNotNull(course.getSubjectCode());
+            Assert.assertNotNull(course.getCode());
+            Assert.assertNotNull(course.getTitle());
+
+            Assert.assertThat(course.getSubjectCode().length(), Matchers.greaterThan(0));
+            Assert.assertThat(course.getCode().length(), Matchers.greaterThan(0));
+            Assert.assertThat(course.getTitle().length(), Matchers.greaterThan(0));
+        }
+    }
+
+    private void assertAllSubjectDataValid(List<Subject> subjects) {
+        for (Subject subject : subjects) {
+            Assert.assertNotNull(subject.getCode());
+            Assert.assertNotNull(subject.getTitle());
+            Assert.assertThat(subject.getCode().length(), Matchers.greaterThan(0));
+            Assert.assertThat(subject.getTitle().length(), Matchers.greaterThan(0));
+        }
+    }
+
+    private void assertAllMeetingTimesValid(List<Meeting> meetings) {
+        for (Meeting meeting : meetings) {
+            Assert.assertThat(meeting.getStartTime(), Matchers.greaterThan(0));
+            Assert.assertThat(meeting.getEndTime(), Matchers.greaterThan(0));
+            Assert.assertThat(meeting.getEndTime(), Matchers.greaterThan(meeting.getStartTime()));
+        }
     }
 }
