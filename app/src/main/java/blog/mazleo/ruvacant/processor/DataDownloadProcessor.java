e8aabbaa47c4a70ca5f9d614c69fdc5780894908
Jonnelin Marzielli Leonardo <jonnelin.leonardo@gmail.com>
2020-11-23 16:19:26 -0500

    Commit initial implementation of DataDownloadProcessor

--- app/src/main/java/blog/mazleo/ruvacant/processor/DataDownloadProcessor.java
+++ app/src/main/java/blog/mazleo/ruvacant/processor/DataDownloadProcessor.java
@@ -1,9 +1,46 @@
 package blog.mazleo.ruvacant.processor;
 
+import android.app.Activity;
+
+import androidx.lifecycle.MutableLiveData;
+
 import blog.mazleo.ruvacant.model.Option;
+import blog.mazleo.ruvacant.viewmodel.CoursesViewModel;
+import blog.mazleo.ruvacant.viewmodel.LocationsViewModel;
 
 public class DataDownloadProcessor {
+    // ----- FIELDS -----
+    private Activity currentActivity;
     private Option selectedOptions;
+
+    // ----- VIEWMODELS -----
+    private LocationsViewModel locationsViewModel;
+    private CoursesViewModel coursesViewModel;
+    // TODO Create
+    // private DatabaseViewModel databaseViewModel;
+
+    // ----- UI Components -----
+    private MutableLiveData<Boolean> dataRetrievalProgress;
+    private MutableLiveData<Boolean> dataDownloadProgress;
+    private MutableLiveData<Boolean> dataSaveProgress;
+
+    public DataDownloadProcessor(Activity currentActivity, Option selectedOptions) {
+        this.currentActivity = currentActivity;
+        this.selectedOptions = selectedOptions;
+
+        this.locationsViewModel = new LocationsViewModel(this);
+        this.coursesViewModel = new CoursesViewModel(this);
+        // TODO DBVM
+        // this.databaseViewModel = new ...
+
+        this.dataRetrievalProgress = new MutableLiveData<>();
+        this.dataDownloadProgress = new MutableLiveData<>();
+        this.dataSaveProgress = new MutableLiveData<>();
+
+        this.dataRetrievalProgress.setValue(false);
+        this.dataDownloadProgress.setValue(false);
+        this.dataSaveProgress.setValue(false);
+    }
 
     public Option getSelectedOptions() {
         return selectedOptions;
@@ -11,5 +48,25 @@ public class DataDownloadProcessor {
 
     public void setSelectedOptions(Option selectedOptions) {
         this.selectedOptions = selectedOptions;
+    }
+
+    public LocationsViewModel getLocationsViewModel() {
+        return locationsViewModel;
+    }
+
+    public CoursesViewModel getCoursesViewModel() {
+        return coursesViewModel;
+    }
+
+    public boolean isDataDownloadInProgress() {
+        return this.dataDownloadProgress.getValue();
+    }
+
+    public boolean isDataSaveInProgress() {
+        return this.dataSaveProgress.getValue();
+    }
+
+    public boolean isDataRetrievalInProgress() {
+        return this.dataRetrievalProgress.getValue();
     }
 }
