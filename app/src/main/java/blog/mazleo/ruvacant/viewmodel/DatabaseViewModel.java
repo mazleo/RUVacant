e78ca56ba11aca726dbc64cd9dc8e66e358ad8c9
Jonnelin Marzielli Leonardo <jonnelin.leonardo@gmail.com>
2020-11-25 18:02:26 -0500

    Implement DatabaseViewModel

---
+++ app/src/main/java/blog/mazleo/ruvacant/viewmodel/DatabaseViewModel.java
@@ -0,0 +1,70 @@
+package blog.mazleo.ruvacant.viewmodel;
+
+import android.app.Activity;
+
+import java.util.List;
+
+import blog.mazleo.ruvacant.model.Building;
+import blog.mazleo.ruvacant.model.Class;
+import blog.mazleo.ruvacant.model.ClassInstructor;
+import blog.mazleo.ruvacant.model.Course;
+import blog.mazleo.ruvacant.model.Instructor;
+import blog.mazleo.ruvacant.model.Meeting;
+import blog.mazleo.ruvacant.model.Option;
+import blog.mazleo.ruvacant.model.Room;
+import blog.mazleo.ruvacant.model.Subject;
+import blog.mazleo.ruvacant.processor.DataDownloadProcessor;
+import blog.mazleo.ruvacant.repository.DatabaseRepository;
+
+public class DatabaseViewModel {
+    private DataDownloadProcessor dataDownloadProcessor;
+    private DatabaseRepository databaseRepository;
+
+    public DatabaseViewModel(Activity activity) {
+        databaseRepository = new DatabaseRepository(this, activity);
+    }
+
+    public void setupInitialDatabase(Option option) {
+        databaseRepository.setupInitialDatabase(option);
+    }
+
+    public void onInitialDatabaseSetupComplete() {
+        // TODO Finish
+    }
+
+    public void saveLocations(List<Building> buildings, List<Room> rooms) {
+        databaseRepository.saveLocations(buildings, rooms);
+    }
+
+    public void onSaveLocationsComplete() {
+        // TODO Finish
+    }
+
+    public void saveCourses(List<Subject> subjects, List<Course> courses, List<Class> classes, List<Instructor> instructors, List<ClassInstructor> classesInstructors, List<Meeting> meetings) {
+        databaseRepository.saveCourses(subjects, courses, classes, instructors, classesInstructors, meetings);
+    }
+
+    public void onSaveCoursesComplete() {
+        // TODO Finish
+    }
+
+    public void cleanUp() {
+        if (databaseRepository != null) {
+            databaseRepository.cleanUp();
+            databaseRepository = null;
+        }
+        dataDownloadProcessor = null;
+    }
+
+    public void cleanUpKeepData() {
+        if (databaseRepository != null) {
+            databaseRepository.cleanUpKeepData();
+            databaseRepository = null;
+        }
+        dataDownloadProcessor = null;
+    }
+
+    public void setDataDownloadProcessor(DataDownloadProcessor dataDownloadProcessor) {
+        this.dataDownloadProcessor = dataDownloadProcessor;
+    }
+}
