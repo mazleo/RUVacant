0cb30e19b2ef13294552b7de4001bfddd0e767a0
Jonnelin Marzielli Leonardo <jonnelin.leonardo@gmail.com>
2020-11-27 20:08:47 -0500

    Reflect download and save process in UI

--- app/src/main/java/blog/mazleo/ruvacant/processor/DataDownloadProcessor.java
+++ app/src/main/java/blog/mazleo/ruvacant/processor/DataDownloadProcessor.java
@@ -24,7 +24,11 @@ public class DataDownloadProcessor {
     // ----- UI Components -----
     private MutableLiveData<Boolean> dataRetrievalProgress;
     private MutableLiveData<Boolean> dataDownloadProgress;
+    private MutableLiveData<Boolean> dataLocationsDownloadProgress;
+    private MutableLiveData<Boolean> dataCoursesDownloadProgress;
     private MutableLiveData<Boolean> dataSaveProgress;
+    private MutableLiveData<Boolean> dataLocationsSaveProgress;
+    private MutableLiveData<Boolean> dataCoursesSaveProgress;
     private MutableLiveData<Boolean> onDownloadError;
     private MutableLiveData<Boolean> onSaveError;
 
@@ -40,13 +44,21 @@ public class DataDownloadProcessor {
 
         this.dataRetrievalProgress = new MutableLiveData<>();
         this.dataDownloadProgress = new MutableLiveData<>();
+        this.dataLocationsDownloadProgress = new MutableLiveData<>();
+        this.dataCoursesDownloadProgress = new MutableLiveData<>();
         this.dataSaveProgress = new MutableLiveData<>();
+        this.dataLocationsSaveProgress = new MutableLiveData<>();
+        this.dataCoursesSaveProgress = new MutableLiveData<>();
         this.onDownloadError = new MutableLiveData<>();
         this.onSaveError = new MutableLiveData<>();
 
         this.dataRetrievalProgress.setValue(false);
         this.dataDownloadProgress.setValue(false);
+        this.dataLocationsDownloadProgress.setValue(false);
+        this.dataCoursesDownloadProgress.setValue(false);
         this.dataSaveProgress.setValue(false);
+        this.dataLocationsSaveProgress.setValue(false);
+        this.dataCoursesSaveProgress.setValue(false);
         this.onDownloadError.setValue(false);
         this.onSaveError.setValue(false);
     }
@@ -57,6 +69,7 @@ public class DataDownloadProcessor {
     }
 
     public void downloadLocations() {
+        dataLocationsDownloadProgress.setValue(true);
         if (locationsViewModel == null) {
             locationsViewModel = new LocationsViewModel(this);
         }
@@ -64,11 +77,13 @@ public class DataDownloadProcessor {
     }
 
     public void onDownloadLocationsComplete() {
+        dataLocationsDownloadProgress.setValue(false);
         downloadCourses();
         errorMessage = null;
     }
 
     public void downloadCourses() {
+        dataCoursesDownloadProgress.setValue(true);
         if (coursesViewModel == null) {
             coursesViewModel = new CoursesViewModel(this);
         }
@@ -76,6 +91,7 @@ public class DataDownloadProcessor {
     }
 
     public void onDownloadCoursesComplete() {
+        dataCoursesDownloadProgress.setValue(false);
         onDownloadComplete();
         errorMessage = null;
     }
@@ -87,15 +103,18 @@ public class DataDownloadProcessor {
     }
 
     public void saveLocations() {
+        dataLocationsSaveProgress.setValue(true);
         databaseViewModel.saveLocations(locationsViewModel.getBuildings(), locationsViewModel.getRooms());
     }
 
     public void onSaveLocationsComplete() {
+        dataLocationsSaveProgress.setValue(false);
         saveCourses();
         errorMessage = null;
     }
 
     public void saveCourses() {
+        dataCoursesSaveProgress.setValue(true);
         databaseViewModel.saveCourses(
                 coursesViewModel.getSubjects(),
                 coursesViewModel.getCourses(),
@@ -107,6 +126,7 @@ public class DataDownloadProcessor {
     }
 
     public void onSaveCoursesComplete() {
+        dataCoursesSaveProgress.setValue(false);
         onSaveComplete();
         errorMessage = null;
     }
@@ -215,5 +235,45 @@ public class DataDownloadProcessor {
 
     public boolean isDataRetrievalInProgress() {
         return this.dataRetrievalProgress.getValue();
+    }
+
+    public MutableLiveData<Boolean> getDataRetrievalProgress() {
+        return dataRetrievalProgress;
+    }
+
+    public MutableLiveData<Boolean> getDataDownloadProgress() {
+        return dataDownloadProgress;
+    }
+
+    public MutableLiveData<Boolean> getDataLocationsDownloadProgress() {
+        return dataLocationsDownloadProgress;
+    }
+
+    public MutableLiveData<Boolean> getDataCoursesDownloadProgress() {
+        return dataCoursesDownloadProgress;
+    }
+
+    public MutableLiveData<Boolean> getDataSaveProgress() {
+        return dataSaveProgress;
+    }
+
+    public MutableLiveData<Boolean> getDataLocationsSaveProgress() {
+        return dataLocationsSaveProgress;
+    }
+
+    public MutableLiveData<Boolean> getDataCoursesSaveProgress() {
+        return dataCoursesSaveProgress;
+    }
+
+    public MutableLiveData<Boolean> getOnDownloadError() {
+        return onDownloadError;
+    }
+
+    public MutableLiveData<Boolean> getOnSaveError() {
+        return onSaveError;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
     }
 }
--- app/src/main/java/blog/mazleo/ruvacant/view/activities/OptionsActivity.java
+++ app/src/main/java/blog/mazleo/ruvacant/view/activities/OptionsActivity.java
@@ -6,32 +6,51 @@ import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.animation.ObjectAnimator;
+import android.app.Activity;
 import android.content.res.ColorStateList;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import blog.mazleo.ruvacant.R;
+import blog.mazleo.ruvacant.model.Option;
+import blog.mazleo.ruvacant.processor.DataDownloadProcessor;
+import blog.mazleo.ruvacant.utils.OptionsUtil;
 import blog.mazleo.ruvacant.view.dialogfragments.OptionsPickerDialogFragment;
 
 public class OptionsActivity extends AppCompatActivity {
 
     private static final String DIALOG_FRAGMENT_TAG = "OPTIONS_DIALOG_FRAGMENT";
+    private Activity activity;
 
     public Button semesterButton;
     public Button campusButton;
     public Button levelButton;
     public Button saveButton;
 
+    private View dimmingView;
+    private ProgressBar dataRetrievalProgressBar;
+    private ImageView dataRetrievalImageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
 
+        this.activity = this;
+
         this.semesterButton = findViewById(R.id.options_semester_button);
         this.campusButton = findViewById(R.id.options_campus_button);
         this.levelButton = findViewById(R.id.options_level_button);
         this.saveButton = findViewById(R.id.options_save_button);
+
+        this.dimmingView = findViewById(R.id.dim_act_options);
+        this.dataRetrievalProgressBar = findViewById(R.id.data_retrieval_progress_bar);
+        this.dataRetrievalImageView = findViewById(R.id.data_retrieval_imageview);
 
         this.semesterButton.setOnClickListener(
                 view -> {
@@ -54,10 +73,117 @@ public class OptionsActivity extends AppCompatActivity {
         this.saveButton.setTextColor(saveButtonTextColors);
         this.saveButton.setOnClickListener(
                 view -> {
-                    // TODO: If saved enabled, process data
-                    Toast.makeText(getApplicationContext(), "SAVE BUTTON CLICKED", Toast.LENGTH_SHORT).show();
+                    String chosenSemester = this.semesterButton.getText().toString();
+                    String chosenCampus = this.campusButton.getText().toString();
+                    String chosenLevel = this.levelButton.getText().toString();
+
+                    int semesterMonth = OptionsUtil.getSemesterMonthFromSemesterString(chosenSemester);
+                    int semesterYear = OptionsUtil.getSemesterYearFromSemesterString(chosenSemester);
+                    String campusCode = OptionsUtil.getSchoolCampusCodeFromName(chosenCampus);
+                    String levelCode = OptionsUtil.getLevelCodeFromName(chosenLevel);
+
+                    Option selectedOptions = new Option(
+                            semesterMonth,
+                            semesterYear,
+                            campusCode,
+                            levelCode);
+
+                    DataDownloadProcessor dataDownloadProcessor = new DataDownloadProcessor(activity, selectedOptions);
+
+                    dataDownloadProcessor.getDataRetrievalProgress().observe(
+                            this,
+                            inProgress -> {
+                                if (inProgress) {
+                                    dataRetrievalProgressBar.setVisibility(View.VISIBLE);
+                                    dataRetrievalImageView.setVisibility(View.VISIBLE);
+                                    dimmingView.setVisibility(View.VISIBLE);
+                                    ObjectAnimator.ofFloat(dimmingView, "alpha", 0f, 1f)
+                                            .setDuration(500)
+                                            .start();
+                                    ObjectAnimator.ofFloat(dataRetrievalProgressBar, "alpha", 0f, 1f)
+                                            .setDuration(500)
+                                            .start();
+                                    ObjectAnimator.ofFloat(dataRetrievalImageView, "alpha", 0f, 1f)
+                                            .setDuration(500)
+                                            .start();
+                                }
+                                else {
+                                    ObjectAnimator.ofFloat(dimmingView, "alpha", 1f, 0f)
+                                            .setDuration(500)
+                                            .start();
+                                    ObjectAnimator.ofFloat(dataRetrievalProgressBar, "alpha", 1f, 0f)
+                                            .setDuration(500)
+                                            .start();
+                                    ObjectAnimator.ofFloat(dataRetrievalImageView, "alpha", 1f, 0f)
+                                            .setDuration(500)
+                                            .start();
+
+                                    dimmingView.setVisibility(View.GONE);
+                                    dataRetrievalProgressBar.setVisibility(View.GONE);
+                                    dataRetrievalImageView.setVisibility(View.GONE);
+                                }
+                            }
+                    );
+
+                    dataDownloadProcessor.getDataLocationsDownloadProgress().observe(
+                            this,
+                            inProgress -> {
+                                if (inProgress) {
+                                    dataRetrievalImageView.setBackground(getDrawable(R.drawable.ic_download_locations));
+                                }
+                            }
+                    );
+                    dataDownloadProcessor.getDataCoursesDownloadProgress().observe(
+                            this,
+                            inProgress -> {
+                                if (inProgress) {
+                                    dataRetrievalImageView.setBackground(getDrawable(R.drawable.ic_download_courses));
+                                }
+                            }
+                    );
+                    dataDownloadProcessor.getDataLocationsSaveProgress().observe(
+                            this,
+                            inProgress -> {
+                                if (inProgress) {
+                                    dataRetrievalImageView.setBackground(getDrawable(R.drawable.ic_save_locations));
+                                }
+                            }
+                    );
+                    dataDownloadProcessor.getDataCoursesSaveProgress().observe(
+                            this,
+                            inProgress -> {
+                                if (inProgress) {
+                                    dataRetrievalImageView.setBackground(getDrawable(R.drawable.ic_save_courses));
+                                }
+                            }
+                    );
+                    dataDownloadProcessor.getOnDownloadError().observe(
+                            this,
+                            hasDownloadError -> {
+                                if (hasDownloadError) {
+                                    String message = dataDownloadProcessor.getErrorMessage();
+                                    if (message != null) {
+                                        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            }
+                    );
+                    dataDownloadProcessor.getOnSaveError().observe(
+                            this,
+                            hasSaveError -> {
+                                if (hasSaveError) {
+                                    String message = dataDownloadProcessor.getErrorMessage();
+                                    if (message != null) {
+                                        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            }
+                    );
+
+                    dataDownloadProcessor.initializeDataDownload();
                 }
         );
+
     }
 
     private void showOptionsPicker(int pickerType) {
