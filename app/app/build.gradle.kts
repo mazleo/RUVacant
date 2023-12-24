c0427b3345652810684147c1c14a40bfe26d9e67
Jonnelin Leonardo <jonnelin.leonardo@gmail.com>
2023-12-24 00:46:54 -0500

    Install dependencies and add internet permission.

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -33,7 +33,9 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0");
     implementation("androidx.room:room-runtime:2.6.1")
+    implementation("com.google.code.gson:gson:2.10.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     annotationProcessor("androidx.room:room-compiler:2.6.1")
     implementation("com.google.dagger:dagger:2.50")
