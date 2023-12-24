4e2a776ed7d368bed80cdb0e1d46f2a1699efc33
Jonnelin Leonardo <jonnelin.leonardo@gmail.com>
2023-12-23 21:47:44 -0500

    Add starter screen

---
+++ app/app/src/main/java/blog/mazleo/ruvacant/ui/splash/SplashActivity.java
@@ -0,0 +1,16 @@
+package blog.mazleo.ruvacant.ui.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import blog.mazleo.ruvacant.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
