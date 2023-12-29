fa1cce15e609be6274d2f38ac56b1a292a8e290e
Jonnelin Leonardo <jonnelin.leonardo@gmail.com>
2023-12-29 16:23:57 -0500

    Allow sharing of cached data.
    TESTED=Run tests. Build app.

---
+++ app/app/src/main/java/blog/mazleo/ruvacant/shared/ApplicationData.java
@@ -0,0 +1,18 @@
+package blog.mazleo.ruvacant.shared;
+
+/** The shared data being held. */
+public enum ApplicationData {
+  SUBJECTS_LIST_CACHE("subjects-list-cache"),
+  SUBJECTS_NUM("subjects-num"),
+  CLASS_INFOS_CACHE("class-infos-cache");
+
+  private final String tag;
+
+  ApplicationData(String tag) {
+    this.tag = tag;
+  }
+
+  public String getTag() {
+    return tag;
+  }
+}
